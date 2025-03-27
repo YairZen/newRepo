40e2f47adcf64a88fa26752fcaeb7613ebcfc32d
GyanLevy <gyan812@gmail.com>
2025-03-27 13:47:02 +0200

    Gyan- JavaDoc

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,42 +1,85 @@
-import java.util.ArrayList;  //**
+import java.util.ArrayList;
 
-public class SharedData 
-{
-	private ArrayList<Integer> array;  //** 
-	private boolean[] winArray;
-	private boolean flag;
-	private final int b;
+/**
+ * SharedData is a data container used for multi-threaded operations.
+ * It stores a list of integers, a target value {@code b}, a win array indicating
+ * matches, and a flag to indicate success.
+ */
+public class SharedData {
 	
-	public SharedData(ArrayList<Integer> array, int b) {  //** 
-		this.array = array;  //** 
+	/** The list of integers to search through. */
+	private ArrayList<Integer> array;
+	
+	/** Boolean array representing which indexes matched the search. */
+	private boolean[] winArray;
+	
+	/** Flag indicating whether a successful match was found. */
+	private boolean flag;
+	
+	/** The target number to search for in the array. */
+	private final int b;
+
+	/**
+	 * Constructs a SharedData object with the given array and target number.
+	 *
+	 * @param array the list of integers to search
+	 * @param b the target number to search for
+	 */
+	public SharedData(ArrayList<Integer> array, int b) {
+		this.array = array;
 		this.b = b;
 	}
 
-	public boolean[] getWinArray() 
-	{
+	/**
+	 * Gets the win array indicating which positions matched.
+	 *
+	 * @return the win array
+	 */
+	public boolean[] getWinArray() {
 		return winArray;
 	}
 
-	public void setWinArray(boolean[] winArray) 
-	{
+	/**
+	 * Sets the win array.
+	 *
+	 * @param winArray the boolean array indicating matching positions
+	 */
+	public void setWinArray(boolean[] winArray) {
 		this.winArray = winArray;
 	}
 
-	public ArrayList<Integer> getArray()  //** 
-	{
-		return array;  //** 
+	/**
+	 * Gets the list of integers.
+	 *
+	 * @return the integer list
+	 */
+	public ArrayList<Integer> getArray() {
+		return array;
 	}
 
-	public int getB() 
-	{
+	/**
+	 * Gets the target number to search for.
+	 *
+	 * @return the target number {@code b}
+	 */
+	public int getB() {
 		return b;
 	}
 
-	public boolean getFlag() 
-	{
+	/**
+	 * Checks if the flag is set (successful match).
+	 *
+	 * @return true if a match was found; false otherwise
+	 */
+	public boolean getFlag() {
 		return flag;
 	}
 
+	/**
+	 * Sets the flag indicating whether a match was found.
+	 *
+	 * @param flag true if a match was found; false otherwise
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
