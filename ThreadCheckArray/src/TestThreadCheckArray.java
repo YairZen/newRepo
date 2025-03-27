a1c17aad9d6110cf51d1259a0de1fd03f4f5cfe8
GyanLevy <gyan812@gmail.com>
2025-03-27 12:59:12 +0200

    fgdfg

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestThreadCheckArray {
@@ -5,51 +6,48 @@ public class TestThreadCheckArray {
 		try (Scanner input = new Scanner(System.in)) {
 			Thread thread1, thread2;
 			System.out.println("Enter array size");
-			int num  = input.nextInt();
-			int [] array = new int[num];
+			int num = input.nextInt();
+			ArrayList<Integer> array = new ArrayList<>();
+
 			System.out.println("Enter numbers for array");
-			
-			for (int index = 0; index < num; index++) 
-				array[index] = input.nextInt();
-			
+			for (int index = 0; index < num; index++)
+				array.add(input.nextInt());
+
 			System.out.println("Enter number");
 			num = input.nextInt();
-			
+
 			SharedData sd = new SharedData(array, num);
-			
+
 			thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
 			thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
 			thread1.start();
 			thread2.start();
-			try 
-			{
+			try {
 				thread1.join();
 				thread2.join();
-			} 
-			catch (InterruptedException e)
-			{
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			if (!sd.getFlag())
-			{
+			if (!sd.getFlag()) {
 				System.out.println("Sorry");
 				return;
 			}
-			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().length);
+			System.out.println("Solution for b : " + sd.getB() + ", n = " + sd.getArray().size());
+
 			System.out.print("I:    ");
-			for(int index = 0; index < sd.getArray().length ; index++)
+			for (int index = 0; index < sd.getArray().size(); index++)
 				System.out.print(index + "    ");
 			System.out.println();
+
 			System.out.print("A:    ");
-			for (int index : sd.getArray())
-			{
-				System.out.print(index);
+			for (int value : sd.getArray()) {
+				System.out.print(value);
 				int counter = 5;
-				while (true)
-				{
-					index = index / 10;
+				int temp = value;
+				while (true) {
+					temp = temp / 10;
 					counter--;
-					if (index == 0)
+					if (temp == 0)
 						break;
 				}
 				for (int i = 0; i < counter; i++)
@@ -58,14 +56,12 @@ public class TestThreadCheckArray {
 
 			System.out.println();
 			System.out.print("C:    ");
-			for (boolean index : sd.getWinArray())
-			{
+			for (boolean index : sd.getWinArray()) {
 				if (index)
 					System.out.print("1    ");
 				else
-					System.out.print("0    ");	
+					System.out.print("0    ");
 			}
 		}
 	}
-
 }
